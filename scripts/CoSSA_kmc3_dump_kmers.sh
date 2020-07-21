9143841d47f06f36f903e31efe93307f7e36c1a5
Danny Esselink <essel002@jacqueline-i.plantbreeding.nl>
2020-07-21 21:01:29 +0200

    added k-mer dump script

---
+++ scripts/CoSSA_kmc3_dump_kmers.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+############################
+# check for usage
+############################
+display_usage()
+{
+   echo -e "\nUsage:\n$0 path_to/kmer-library\n" 1>&2
+}
+# if less than required arguments supplied, display usage
+if [  $# -ne 2 ]
+then
+   display_usage
+   exit 1
+fi
+# check whether user had supplied -h or --help . If yes display usage
+if [[ ( $# == "--help") ||  $# == "-h" ]]
+then
+   display_usage
+   exit 0
+fi
+
+##########################################
+# subtraction of kmers
+##########################################
+# in case the sample name with the kmc extension is given
+infile1=$(echo "$1" | sed 's/\.kmc_...//')
+
+### run kmc_tools substract kmers of sample1 with kmers of sample2 to obtain sample1 specific kmers
+kmc_tools transform ${sample} dump ${sample}.dump
